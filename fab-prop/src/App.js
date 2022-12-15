d9f18faf3f17fede4e18d98daf53202e5b60b171
Chris Jacobi <106036496+ChrisJacobi@users.noreply.github.com>
2022-12-14 21:25:01 -0500

    contact form frontend work

--- fab-prop/src/App.js
+++ fab-prop/src/App.js
@@ -1,15 +1,21 @@
-
+import React from 'react';
 import './App.css';
 
+// APP COMPONENT
 function App() {
   return (
     <div className="App">
       <Header />
+      <Info />
       <Contact />
     </div>
   );
 }
 
+
+
+
+// START HEADER COMPONENT
 function Header() {
   return (
     <header className="App-header">
@@ -19,35 +25,82 @@ function Header() {
     </header>
   );
 }
+// END HEADER COMPONENT
 
-function Contact() {
+
+// START INFO COMPONENT
+function Info(){
   return (
-    <div className="Contact-form">
-      Get in touch
-      <Form />
-     <div className="Contact-info">
-      Links: airbnb, facebook
-     </div>
-    </div>
+    <section className="info-container">
+      <article>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Urna porttitor rhoncus dolor purus non enim praesent elementum facilisis. Tortor consequat id porta nibh. At varius vel pharetra vel. 
+        Scelerisque eleifend donec pretium vulputate sapien nec sagittis. Turpis tincidunt id aliquet risus feugiat in. Ultricies lacus sed turpis tincidunt id. 
+        Mauris augue neque gravida in fermentum et sollicitudin. Auctor elit sed vulputate mi sit amet mauris commodo quis. Aliquet nibh praesent tristique magna sit amet purus. 
+        Justo donec enim diam vulputate ut pharetra sit. Eu scelerisque felis imperdiet proin fermentum leo vel. Lacus sed viverra tellus in hac. 
+        Tortor consequat id porta nibh venenatis cras sed felis. Posuere morbi leo urna molestie at. Egestas egestas fringilla phasellus faucibus. 
+        Eget nunc scelerisque viverra mauris. Odio ut sem nulla pharetra diam.
+
+        Nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue. Imperdiet massa tincidunt nunc pulvinar sapien et. 
+        Convallis a cras semper auctor neque vitae tempus quam. Libero nunc consequat interdum varius sit amet. Dignissim cras tincidunt lobortis feugiat vivamus. 
+        In est ante in nibh mauris cursus mattis molestie a. Fermentum et sollicitudin ac orci phasellus egestas tellus rutrum. 
+        Enim sit amet venenatis urna. Eget mauris pharetra et ultrices neque ornare. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed. 
+        Vitae proin sagittis nisl rhoncus. Fringilla est ullamcorper eget nulla facilisi.
+      </article>
+    </section>
   );
 }
+// END INFO COMPONENT
 
-function Form() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log()
+
+// START CONTACT FORM COMPONENT
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
   }
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state)
+    const resetForm = () => {
+      this.setState({name: '', email: '', message:''})
+    }
+    resetForm()
+  }
+  render(){
   return (
-    <form className="contactForm" onSubmit={handleSubmit}>
-      <label className="labels" for="nameInput">Name</label>
-        <input name="nameInput" type="text" className="inputs" placeholder="First and Last name" required />
-      <label className="labels" for="emailInput">Email</label>
-        <input name="emailInput" type="email" className="inputs" placeholder="Email" required />
-      <label className="labels" for="messageInput">Message</label>
-        <textarea name="messageInput" className="inputs" placeholder="Enter message" rows="5" cols="40" required ></textarea>
+    <div className="contact-form-container">
+      Get in touch
+    <form className="contactForm" onSubmit={this.handleSubmit.bind(this)}>
+      <label className="labels" htmlFor="nameInput">Name</label>
+        <input name="nameInput" type="text" className="inputs" placeholder="First and Last name" value={this.state.name} onChange={this.onNameChange.bind(this)} required />
+      <label className="labels" htmlFor="emailInput">Email</label>
+        <input name="emailInput" type="email" className="inputs" placeholder="Email" value={this.state.email} onChange={this.onEmailChange.bind(this)} required />
+      <label className="labels" htmlFor="messageInput">Message</label>
+        <textarea name="messageInput" className="inputs" placeholder="Enter message" rows="5" cols="40" value={this.state.message} onChange={this.onMessageChange.bind(this)} required />
       <button type="submit" id="sendBtn">Send</button>
     </form>
-  );
+      <div className="Contact-info">
+        Links: airbnb, facebook
+      </div>
+    </div>
+    );
+  }
+  onNameChange = (event) => {
+    this.setState({name: event.target.value})
+  }
+  onEmailChange = (event) => {
+    this.setState({email: event.target.value})
+  }
+  onMessageChange = (event) => {
+    this.setState({message: event.target.value})
+  }
 }
+// END CONTACT FORM COMPONENT
+
 
 export default App;
